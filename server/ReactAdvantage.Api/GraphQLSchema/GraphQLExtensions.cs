21fb9716e672c27abc96de42a02b2b8d9fd81c35
Aleksey Glukharev <neogarrigus@gmail.com>
2018-08-05 04:24:07 -0400

    Added functionality to query the user by FirstName, LastName, Name, or Email

---
+++ server/ReactAdvantage.Api/GraphQLSchema/GraphQLExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ReactAdvantage.Api.GraphQLSchema
+{
+    public static class GraphQLExtensions
+    {
+        public static IQueryable<TQuery> HandleQueryArgument<TQuery, TArgument>(this IQueryable<TQuery> query, ArgumentGetter<TArgument> argumentGetter, Func<TArgument, IQueryable<TQuery>, IQueryable<TQuery>> handler)
+        {
+            if (!argumentGetter.HasArgument())
+            {
+                return query;
+            }
+
+            var argumentValue = argumentGetter.GetArgument();
+
+            return handler(argumentValue, query);
+        }
+    }
+}
--- server/ReactAdvantage.Api/GraphQLSchema/ReactAdvantageQuery.cs
+++ server/ReactAdvantage.Api/GraphQLSchema/ReactAdvantageQuery.cs
@@ -27,16 +27,27 @@ namespace ReactAdvantage.Api.GraphQLSchema
                 resolve: context => db.Users
             );
 
-            // TODO: Need to create a composable query that can include any of Id, FirstName, LastName,  
-            // Email,Name to query the user. Note that some of these arguments may be null and query needs 
-            // to handle that in a performant manner.
-            //Field<ListGraphType<UserType>>(
-            //    "users",
-            //    arguments: new QueryArguments{
-            //        new QueryArgument<IntGraphType> { Name = "id" }
-            //    },
-            //    resolve: context => db.Users.Where(u => u.Id == context.GetArgument<int>("id"))
-            //);
+            Field<ListGraphType<UserType>>(
+                "users",
+                arguments: new QueryArguments(
+                    new QueryArgument<IntGraphType> { Name = "id" },
+                    new QueryArgument<StringGraphType> { Name = "firstname" },
+                    new QueryArgument<StringGraphType> { Name = "lastname" },
+                    new QueryArgument<StringGraphType> { Name = "name" },
+                    new QueryArgument<StringGraphType> { Name = "email" }
+                ),
+                resolve: context => db.Users
+                    .HandleQueryArgument(new ArgumentGetter<int>("id", context), (arg, query) => 
+                        query.Where(x => x.Id == arg))
+                    .HandleQueryArgument(new ArgumentGetter<string>("firstname", context), (arg, query) => 
+                        string.IsNullOrEmpty(arg) ? query : query.Where(x => x.FirstName.Contains(arg)))
+                    .HandleQueryArgument(new ArgumentGetter<string>("lastname", context), (arg, query) =>
+                        string.IsNullOrEmpty(arg) ? query : query.Where(x => x.LastName.Contains(arg)))
+                    .HandleQueryArgument(new ArgumentGetter<string>("name", context), (arg, query) =>
+                        string.IsNullOrEmpty(arg) ? query : query.Where(x => x.Name.Contains(arg)))
+                    .HandleQueryArgument(new ArgumentGetter<string>("email", context), (arg, query) =>
+                        string.IsNullOrEmpty(arg) ? query : query.Where(x => x.Email.Contains(arg)))
+            );
 
             Field<ListGraphType<ProjectType>>(
                 "allprojects",
