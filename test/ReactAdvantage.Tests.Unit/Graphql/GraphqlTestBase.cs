a841ce63233fd70f2ef24b8284d565b0c5f42704
Aleksey Glukharev <neogarrigus@gmail.com>
2018-08-25 02:42:11 -0400

    Added unit tests for graphql users query

--- test/ReactAdvantage.Tests.Unit/Graphql/GraphqlTestBase.cs
+++ test/ReactAdvantage.Tests.Unit/Graphql/GraphqlTestBase.cs
@@ -1,19 +1,25 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using GraphQL;
 using GraphQL.Types;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ReactAdvantage.Api.GraphQLSchema;
 using ReactAdvantage.Data;
+using Xunit;
 
 namespace ReactAdvantage.Tests.Unit.Graphql
 {
     public class GraphqlTestBase
     {
         private readonly ServiceProvider _serviceProvider;
+        private readonly string _databaseName;
 
         public GraphqlTestBase()
         {
+            _databaseName = Guid.NewGuid().ToString();
+
             var services = new ServiceCollection();
 
             services.AddTransient<ReactAdvantageContext>(x => GetInMemoryDbContext());
@@ -42,10 +48,54 @@ namespace ReactAdvantage.Tests.Unit.Graphql
         protected ReactAdvantageContext GetInMemoryDbContext()
         {
             var options = new DbContextOptionsBuilder<ReactAdvantageContext>()
-                .UseInMemoryDatabase(databaseName: "ReactAdvantage")
+                .UseInMemoryDatabase(databaseName: _databaseName)
                 .Options;
 
             return new ReactAdvantageContext(options);
+        }
+        
+        protected void AssertValidGraphqlExecutionResult(ExecutionResult result)
+        {
+            Assert.NotNull(result);
+            Assert.False(result.Errors?.Count > 0, "Graphql result should contain no errors");
+            Assert.NotNull(result.Data);
+        }
+
+        protected void AssertGraphqlResultDictionary(object dataObject, params Action<KeyValuePair<string, object>>[] elementInspectors)
+        {
+            var data = Assert.IsType<Dictionary<string, object>>(dataObject);
+            Assert.Collection(data, elementInspectors);
+        }
+
+        protected void AssertGraphqlResultArray(object collectionObject, params Action<Dictionary<string, object>>[] elementInspectors)
+        {
+            AssertGraphqlResultArray<Dictionary<string, object>>(collectionObject, elementInspectors);
+        }
+
+        protected void AssertGraphqlResultArray<TArrayElement>(object collectionObject, params Action<TArrayElement>[] elementInspectors)
+        {
+            var collectionOfObjects = Assert.IsType<object[]>(collectionObject);
+
+            var collection = new List<TArrayElement>();
+            foreach (var elementObject in collectionOfObjects)
+            {
+                var element = Assert.IsType<TArrayElement>(elementObject);
+                collection.Add(element);
+            }
+
+            Assert.Collection(collection, elementInspectors);
+        }
+
+        protected void AssertPairEqual<TValue>(KeyValuePair<string, object> field, string fieldName, TValue fieldValue)
+        {
+            Assert.Equal(fieldName, field.Key);
+            Assert.Equal(fieldValue, field.Value);
+        }
+
+        protected void AssertPairEqual(KeyValuePair<string, object> field, string fieldName, Action<object> valueInspector)
+        {
+            Assert.Equal(fieldName, field.Key);
+            valueInspector(field.Value);
         }
     }
 }
--- test/ReactAdvantage.Tests.Unit/Graphql/ReactAdvantageQueryShould.cs
+++ test/ReactAdvantage.Tests.Unit/Graphql/ReactAdvantageQueryShould.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using ReactAdvantage.Api.GraphQLSchema;
 using ReactAdvantage.Domain.Models;
 using Xunit;
@@ -7,42 +7,170 @@ namespace ReactAdvantage.Tests.Unit.Graphql
 {
     public class ReactAdvantageQueryShould : GraphqlTestBase
     {
-        [Fact]
-        public async void ReturnUser()
+        public ReactAdvantageQueryShould()
         {
+            // Given
             using (var db = GetInMemoryDbContext())
             {
-                db.Users.Add(new User { Id = 1, Name = "Admin" });
-                db.Users.Add(new User { Id = 2, Name = "Test" });
+                db.Users.Add(new User { Id = 1, Name = "BobRay1", FirstName = "Bob", LastName = "Ray", Email = "BobRay@test.com", IsActive = true });
+                db.Users.Add(new User { Id = 2, Name = "BobRay2", FirstName = "Bob", LastName = "Ray", Email = "BobRay@test.com", IsActive = false });
+                db.Users.Add(new User { Id = 3, Name = "BobRay3", FirstName = "Bob", LastName = "Ray", Email = "BobRay@test2.com", IsActive = true });
+                db.Users.Add(new User { Id = 4, Name = "BobRay4", FirstName = "Bob", LastName = "Ray", Email = "BobRay@test2.com", IsActive = false });
+                db.Users.Add(new User { Id = 5, Name = "BobSmith1", FirstName = "Bob", LastName = "Smith", Email = "BobSmith@test.com", IsActive = true });
+                db.Users.Add(new User { Id = 6, Name = "BobSmith2", FirstName = "Bob", LastName = "Smith", Email = "BobSmith@test.com", IsActive = false });
+                db.Users.Add(new User { Id = 7, Name = "BobSmith3", FirstName = "Bob", LastName = "Smith", Email = "BobSmith@test2.com", IsActive = true });
+                db.Users.Add(new User { Id = 8, Name = "BobSmith4", FirstName = "Bob", LastName = "Smith", Email = "BobSmith@test2.com", IsActive = false });
+                db.Users.Add(new User { Id = 9, Name = "BarbaraRay1", FirstName = "Barbara", LastName = "Ray", Email = "BarbaraRay@test.com", IsActive = true });
+                db.Users.Add(new User { Id = 10, Name = "BarbaraRay2", FirstName = "Barbara", LastName = "Ray", Email = "BarbaraRay@test.com", IsActive = false });
+                db.Users.Add(new User { Id = 11, Name = "BarbaraRay3", FirstName = "Barbara", LastName = "Ray", Email = "BarbaraRay@test2.com", IsActive = true });
+                db.Users.Add(new User { Id = 12, Name = "BarbaraRay4", FirstName = "Barbara", LastName = "Ray", Email = "BarbaraRay@test2.com", IsActive = false });
+                db.Users.Add(new User { Id = 13, Name = "BarbaraSmith1", FirstName = "Barbara", LastName = "Smith", Email = "BarbaraSmith@test.com", IsActive = true });
+                db.Users.Add(new User { Id = 14, Name = "BarbaraSmith2", FirstName = "Barbara", LastName = "Smith", Email = "BarbaraSmith@test.com", IsActive = false });
+                db.Users.Add(new User { Id = 15, Name = "BarbaraSmith3", FirstName = "Barbara", LastName = "Smith", Email = "BarbaraSmith@test2.com", IsActive = true });
+                db.Users.Add(new User { Id = 16, Name = "BarbaraSmith4", FirstName = "Barbara", LastName = "Smith", Email = "BarbaraSmith@test2.com", IsActive = false });
                 db.SaveChanges();
             }
+        }
 
+        [Fact]
+        public async void ReturnUserWithFewFields()
+        {
+            // When
             var result = await BuildSchemaAndExecuteQueryAsync(new GraphQLQuery
             {
                 Query = "query { user(id: 1) { id name } }"
             });
 
-            Assert.NotNull(result);
-            Assert.False(result.Errors?.Count > 0, "Result should contain no errors");
-            Assert.NotNull(result.Data);
-            var data = Assert.IsType<Dictionary<string, object>>(result.Data);
-            var dataElement = Assert.Single(data);
-            Assert.Equal("user", dataElement.Key);
-            var userFields = Assert.IsType<Dictionary<string, object>>(dataElement.Value);
-            Assert.Equal(2, userFields.Count);
-            Assert.Collection(userFields,
-                id => 
-                {
-                    Assert.Equal("id", id.Key);
-                    Assert.Equal(1, id.Value);
-                },
-                name =>
-                {
-                    Assert.Equal("name", name.Key);
-                    Assert.Equal("Admin", name.Value);
-                });
+            // Then
+            AssertValidGraphqlExecutionResult(result);
+
+            AssertGraphqlResultDictionary(result.Data,
+                userResult => AssertPairEqual(userResult, 
+                    "user", user => AssertGraphqlResultDictionary(user,
+                        field => AssertPairEqual(field, "id", 1),
+                        field => AssertPairEqual(field, "name", "BobRay1")
+                    )
+                )
+            );
         }
 
-        
+        [Fact]
+        public async void ReturnUserWithMoreFields()
+        {
+            // When
+            var result = await BuildSchemaAndExecuteQueryAsync(new GraphQLQuery
+            {
+                Query = "query { user(id: 16) { id name firstName lastName email isActive } }"
+            });
+
+            // Then
+            AssertValidGraphqlExecutionResult(result);
+
+            AssertGraphqlResultDictionary(result.Data,
+                userResult => AssertPairEqual(userResult,
+                    "user", user => AssertGraphqlResultDictionary(user,
+                        field => AssertPairEqual(field, "id", 16),
+                        field => AssertPairEqual(field, "name", "BarbaraSmith4"),
+                        field => AssertPairEqual(field, "firstName", "Barbara"),
+                        field => AssertPairEqual(field, "lastName", "Smith"),
+                        field => AssertPairEqual(field, "email", "BarbaraSmith@test2.com"),
+                        field => AssertPairEqual(field, "isActive", false)
+                    )
+                )
+            );
+        }
+
+        [Fact]
+        public async void ReturnQueriedUsers()
+        {
+            // When
+            var result = await BuildSchemaAndExecuteQueryAsync(new GraphQLQuery
+            {
+                Query = "query { users(firstname: \"Bo\", lastname: \"Sm\", email: \"@test2.com\", isactive: true) { id name firstName lastName email isActive } }"
+            });
+
+            // Then
+            AssertValidGraphqlExecutionResult(result);
+
+            AssertGraphqlResultDictionary(result.Data,
+                usersResult => AssertPairEqual(usersResult,
+                    "users", users => AssertGraphqlResultArray(users, 
+                        user => AssertGraphqlResultDictionary(user,
+                            field => AssertPairEqual(field, "id", 7),
+                            field => AssertPairEqual(field, "name", "BobSmith3"),
+                            field => AssertPairEqual(field, "firstName", "Bob"),
+                            field => AssertPairEqual(field, "lastName", "Smith"),
+                            field => AssertPairEqual(field, "email", "BobSmith@test2.com"),
+                            field => AssertPairEqual(field, "isActive", true)
+                        )
+                    )
+                )
+            );
+        }
+
+        [Fact]
+        public async void ReturnQueriedUsersById()
+        {
+            // When
+            var result = await BuildSchemaAndExecuteQueryAsync(new GraphQLQuery
+            {
+                Query = "query { users(id: [1, 2, 3], isactive: true, ) { id name firstName lastName email isActive } }"
+            });
+
+            // Then
+            AssertValidGraphqlExecutionResult(result);
+
+            AssertGraphqlResultDictionary(result.Data,
+                userResult => AssertPairEqual(userResult,
+                    "users", users => AssertGraphqlResultArray(users,
+                        user => AssertGraphqlResultDictionary(user,
+                            field => AssertPairEqual(field, "id", 1),
+                            field => AssertPairEqual(field, "name", "BobRay1"),
+                            field => AssertPairEqual(field, "firstName", "Bob"),
+                            field => AssertPairEqual(field, "lastName", "Ray"),
+                            field => AssertPairEqual(field, "email", "BobRay@test.com"),
+                            field => AssertPairEqual(field, "isActive", true)
+                        ),
+                        user => AssertGraphqlResultDictionary(user,
+                            field => AssertPairEqual(field, "id", 3),
+                            field => AssertPairEqual(field, "name", "BobRay3"),
+                            field => AssertPairEqual(field, "firstName", "Bob"),
+                            field => AssertPairEqual(field, "lastName", "Ray"),
+                            field => AssertPairEqual(field, "email", "BobRay@test2.com"),
+                            field => AssertPairEqual(field, "isActive", true)
+                        )
+                    )
+                )
+            );
+        }
+
+        [Fact]
+        public async void ReturnAllUsers()
+        {
+            // When
+            var result = await BuildSchemaAndExecuteQueryAsync(new GraphQLQuery
+            {
+                Query = "query { users { id name } }"
+            });
+
+            // Then
+            AssertValidGraphqlExecutionResult(result);
+
+            AssertGraphqlResultDictionary(result.Data,
+                userResult => AssertPairEqual(userResult,
+                    "users", users =>
+                    {
+                        var usersArray = Assert.IsType<object[]>(users);
+                        Assert.Equal(16, usersArray.Length);
+                        AssertGraphqlResultDictionary(usersArray.First(),
+                            field => AssertPairEqual(field, "id", 1),
+                            field => AssertPairEqual(field, "name", "BobRay1")
+                        );
+                    }
+                )
+            );
+        }
+
+
     }
 }
