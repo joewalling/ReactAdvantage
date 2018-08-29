a37b66dcfe637130d0c5d9163c28fe5317c66a7b
Aleksey Glukharev <neogarrigus@gmail.com>
2018-08-29 10:59:32 -0400

    Added more GraphqlController unit tests

--- test/ReactAdvantage.Tests.Unit/Api/Controllers/GraphQLControllerShould.cs
+++ test/ReactAdvantage.Tests.Unit/Api/Controllers/GraphQLControllerShould.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using GraphQL;
 using GraphQL.Types;
 using Microsoft.AspNetCore.Mvc;
@@ -13,15 +14,18 @@ namespace ReactAdvantage.Tests.Unit.Api.Controllers
     public class GraphQLControllerShould
     {
         private readonly GraphQLController _graphqlController;
+        private readonly Mock<ISchema> _schemaMock;
+        private readonly Mock<IDocumentExecuter> _documentExecutorMock;
+        private readonly Mock<ILogger<GraphQLController>> _loggerMock;
 
         public GraphQLControllerShould()
         {
             // Given
-            var documentExecutor = new Mock<IDocumentExecuter>();
-            documentExecutor.Setup(x => x.ExecuteAsync(It.IsAny<ExecutionOptions>())).Returns(Task.FromResult(new ExecutionResult()));
-            var schema = new Mock<ISchema>();
-            var logger = new Mock<ILogger<GraphQLController>>();
-            _graphqlController = new GraphQLController(schema.Object, documentExecutor.Object, logger.Object);
+            _documentExecutorMock = new Mock<IDocumentExecuter>();
+            _documentExecutorMock.Setup(x => x.ExecuteAsync(It.IsAny<ExecutionOptions>())).Returns(Task.FromResult(new ExecutionResult()));
+            _schemaMock = new Mock<ISchema>();
+            _loggerMock = new Mock<ILogger<GraphQLController>>();
+            _graphqlController = new GraphQLController(_schemaMock.Object, _documentExecutorMock.Object, _loggerMock.Object);
         }
 
         [Fact]
@@ -38,6 +42,43 @@ namespace ReactAdvantage.Tests.Unit.Api.Controllers
             var okObjectResult = Assert.IsType<OkObjectResult>(result);
             var executionResult = okObjectResult.Value;
             Assert.NotNull(executionResult);
+            Assert.IsType<ExecutionResult>(executionResult);
+        }
+
+        [Fact]
+        public async void ThrowArgumentNullException()
+        {
+            // Then
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                // When
+                await _graphqlController.Post(null);
+            });
+        }
+
+        [Fact]
+        public async void ReturnBadResultOnError()
+        {
+            //Given
+            var expectedExecutionResult = new ExecutionResult
+            {
+                Errors = new ExecutionErrors { new ExecutionError("", new Exception()) }
+            };
+
+            _documentExecutorMock
+                .Setup(x => x.ExecuteAsync(It.IsAny<ExecutionOptions>()))
+                .Returns(Task.FromResult(expectedExecutionResult));
+
+            var query = new GraphQLQuery { Query = @"{ query { users { id name } } }" };
+
+            // When
+            var result = await _graphqlController.Post(query);
+
+            // Then
+            Assert.NotNull(result);
+            var badResult = Assert.IsType<BadRequestObjectResult>(result);
+            var executionResult = badResult.Value;
+            Assert.Equal(expectedExecutionResult, executionResult);
         }
     }
 }
